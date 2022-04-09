5c52b92b8bde9fd231b2bc61b2b64c7ab13161d4
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2022-04-09 01:30:03 -0300

    flutter create 2.8.1

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:exemplo_flutter_versoes/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
